1f9bc5b1b2db36f46d89bada40c1a257766236e9
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2023-09-18 20:46:53 -0700

    remembering again

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,7 @@
-def parrot():
-    pass
+# The parrot() function should accept an argument of a string and both print() and return the string at the end of the function.
+
+# The parrot() function should have a default argument of "Squawk!"
+
+def parrot(talk="Squawk!"):
+    print(talk)
+    return talk
